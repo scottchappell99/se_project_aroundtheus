15b7a5df87d01388eed5424c82ed5306a6c3b01e
scott.chappell <scott.chappell99@gmail.com>
2024-05-02 13:01:47 -0400

    Fixed some styling and javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -27,43 +27,53 @@ let initialCards = [
 
 const modal = document.querySelector(".modal");
 const profile = document.querySelector(".profile");
-let profileName = profile.querySelector(".profile__name");
+const profileName = profile.querySelector(".profile__name");
 const editButton = profile.querySelector(".profile__edit-button");
-let profileSubtitle = profile.querySelector(".profile__subtitle");
+const profileSubtitle = profile.querySelector(".profile__subtitle");
 const modalClose = modal.querySelector(".modal__close");
-let modalName = modal.querySelector(".modal__name");
-let modalSubtitle = modal.querySelector(".modal__subtitle");
+const modalName = modal.querySelector(".modal__name");
+const modalSubtitle = modal.querySelector(".modal__subtitle");
 const modalSave = modal.querySelector(".modal__button");
 const picturesList = document.querySelector(".pictures__list");
-let cardTemplate = document.querySelector("#card-template").content;
+const cardTemplate = document.querySelector("#card-template").content;
 
-//Saving the profile's edits
+// Opening the modal and pulling the name and job from the page
+function openModal() {
+  modalName.value = profileName.textContent;
+  modalSubtitle.value = profileSubtitle.textContent;
+  modal.classList.add("modal_opened");
+}
+
+// Closing the modal
+function closeModal() {
+  modal.classList.remove("modal_opened");
+}
+
+// Saving the profile's edits
 function saveProfile(event) {
   event.preventDefault();
   profileName.textContent = modalName.value;
   profileSubtitle.textContent = modalSubtitle.value;
-  modal.classList.remove("modal_opened");
+  closeModal();
 }
 
-//Opening the modal and pulling the name and job from the page
-editButton.addEventListener("click", function (openModal) {
-  modalName.value = profileName.textContent;
-  modalSubtitle.value = profileSubtitle.textContent;
-  modal.classList.add("modal_opened");
-});
+//Creating a card
+function createCard(cardNumber) {
+  cardContent = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardContent.querySelector(".card__image");
+  const cardCaption = cardContent.querySelector(".card__caption");
+  cardImage.src = cardNumber.link;
+  cardImage.alt = cardNumber.name;
+  cardCaption.textContent = cardNumber.name;
+  picturesList.append(cardContent);
+}
 
-//Closing the modal with no saves
-modalClose.addEventListener("click", function (closeModal) {
-  modal.classList.remove("modal_opened");
-});
+editButton.addEventListener("click", openModal);
 
-modalSave.addEventListener("click", saveProfile);
+modalClose.addEventListener("click", closeModal);
+
+modalSave.addEventListener("submit", saveProfile);
 
 for (let i = 0; i < initialCards.length; i++) {
-  let cardContent = cardTemplate.querySelector(".card").cloneNode(true);
-  cardContent.querySelector(".card__image").src = initialCards[i].link;
-  cardContent.querySelector(".card__image").alt = initialCards[i].name;
-  cardContent.querySelector(".card__caption").textContent =
-    initialCards[i].name;
-  picturesList.append(cardContent);
+  const cardContent = createCard(initialCards[i]);
 }
