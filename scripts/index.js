abf1859bdde757d9e301beee7239a3158cc66123
scott.chappell <scott.chappell99@gmail.com>
2024-05-08 17:05:47 -0400

    refactoring and fixing of html and javascript

--- scripts/index.js
+++ scripts/index.js
@@ -25,42 +25,39 @@ const initialCards = [
   },
 ];
 
-const modalEdit = document.querySelector(".modal__edit");
+const modalEdit = document.querySelector("#modal-edit");
 const profile = document.querySelector(".profile");
 const profileName = profile.querySelector(".profile__name");
 const editButton = profile.querySelector(".profile__edit-button");
 const profileSubtitle = profile.querySelector(".profile__subtitle");
 const editModalClose = modalEdit.querySelector(".modal__close");
-const editModalForm = modalEdit.querySelector(".modal__form");
+const editModalForm = document.forms["profile-edit"];
 const editModalName = modalEdit.querySelector(".modal__name");
 const editModalSubtitle = modalEdit.querySelector(".modal__subtitle");
 const picturesList = document.querySelector(".pictures__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
-const modalAdd = document.querySelector(".modal__add");
+const modalAdd = document.querySelector("#modal-add");
 const addButton = profile.querySelector(".profile__add-button");
 const addModalClose = modalAdd.querySelector(".modal__close");
-const addModalForm = modalAdd.querySelector(".modal__form");
+const addModalForm = document.forms["picture-add"];
 const addModalImageName = modalAdd.querySelector(".modal__place_type_name");
 const addModalImageLink = modalAdd.querySelector(".modal__place_type_image");
-const bigPopup = document.querySelector(".modal__picture");
+const bigPopup = document.querySelector("#modal-picture");
 const bigPopupContainer = bigPopup.querySelector(".modal__image-container");
 const bigPopupClose = bigPopupContainer.querySelector(".modal__close");
 const bigPopupImage = bigPopupContainer.querySelector(".modal__image");
 const bigPopupCaption = bigPopupContainer.querySelector(".modal__text");
+const closeButtons = document.querySelectorAll(".modal__close");
 
-// Opening the modal
-function handleOpenModal(modal) {
-  editModalName.value = profileName.textContent;
-  editModalSubtitle.value = profileSubtitle.textContent;
+// Opening a modal
+function openModal(modal) {
   modal.classList.add("modal_opened");
 }
 
-// Closing the modal
-function handleCloseModal(modal) {
+// Closing a modal
+function closeModal(modal) {
   modal.classList.remove("modal_opened");
-  addModalImageName.value = "";
-  addModalImageLink.value = "";
 }
 
 // Saving the profile's edits
@@ -68,7 +65,8 @@ function handleSaveProfile(event) {
   event.preventDefault();
   profileName.textContent = editModalName.value;
   profileSubtitle.textContent = editModalSubtitle.value;
-  handleCloseModal(modalEdit);
+  closeModal(modalEdit);
+  event.target.reset();
 }
 
 // Rendering cards
@@ -89,61 +87,65 @@ function handleSaveImage(event) {
       name: addModalImageName.value,
       link: addModalImageLink.value,
     };
-    handleCloseModal(modalAdd);
+    closeModal(modalAdd);
+    event.target.reset();
     renderCard(addImage, picturesList, "before");
   } else {
-    handleCloseModal(modalAdd);
+    closeModal(modalAdd);
   }
 }
 
-//Creating a card
+// Creating a card
 function getCardElement(data) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardCaption = cardElement.querySelector(".card__caption");
   const cardImage = cardElement.querySelector(".card__image");
-  const likeButtons = cardElement.querySelectorAll(".card__like");
+  const likeButton = cardElement.querySelector(".card__like");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardCaption.textContent = data.name;
 
-  //Adding a like button listener to the card
-  likeButtons.forEach((likeButton) => {
-    likeButton.addEventListener("click", () => {
-      likeButton.classList.toggle("card__liked_active");
-    });
+  // Adding a like button listener to the card
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__liked_active");
   });
 
-  //Adding a delete button listener to the card
-  const deleteButton = cardElement.querySelectorAll(".card__delete-button");
+  // Adding a delete button listener to the card
 
-  deleteButton.forEach((deleteButton) => {
-    deleteButton.addEventListener("click", (event) => {
-      const cardToDelete = event.target.closest(".card");
-      cardToDelete.remove();
-    });
+  deleteButton.addEventListener("click", (event) => {
+    const cardToDelete = event.target.closest(".card");
+    cardToDelete.remove();
   });
 
-  //Adding a big popup listener to the card
+  // Adding a big popup listener to the card
 
   cardImage.addEventListener("click", () => {
     bigPopupImage.src = cardImage.src;
     bigPopupImage.alt = cardImage.alt;
     bigPopupCaption.textContent = cardCaption.textContent;
-    handleOpenModal(bigPopup);
+    openModal(bigPopup);
   });
 
   return cardElement;
 }
 
-//Listen for Events
-editButton.addEventListener("click", () => handleOpenModal(modalEdit));
-editModalClose.addEventListener("click", () => handleCloseModal(modalEdit));
-editModalForm.addEventListener("submit", handleSaveProfile);
-addButton.addEventListener("click", () => handleOpenModal(modalAdd));
-addModalClose.addEventListener("click", () => handleCloseModal(modalAdd));
-addModalForm.addEventListener("submit", handleSaveImage);
-bigPopupClose.addEventListener("click", () => handleCloseModal(bigPopup));
+// Listen for Events
+editButton.addEventListener("click", () => {
+  openModal(modalEdit);
+  editModalName.value = profileName.textContent;
+  editModalSubtitle.value = profileSubtitle.textContent;
+});
 
-//Rendering cards
+closeButtons.forEach((button) => {
+  const modal = button.closest(".modal");
+  button.addEventListener("click", () => closeModal(modal));
+});
+
+editModalForm.addEventListener("submit", handleSaveProfile);
+addButton.addEventListener("click", () => openModal(modalAdd));
+addModalForm.addEventListener("submit", handleSaveImage);
+
+// Initial rendering of cards
 initialCards.forEach((data) => renderCard(data, picturesList, "after"));
